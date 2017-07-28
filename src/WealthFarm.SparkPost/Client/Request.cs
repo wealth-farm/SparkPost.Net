7b1e1fdcbaf515e290987f8c9b954f3f83d80a64
Daniel Merritt <dan@Daniels-MacBook-Pro.local>
2017-07-29 04:11:41 +0900

    Add doc comments to Request

--- src/WealthFarm.SparkPost/Client/Request.cs
+++ src/WealthFarm.SparkPost/Client/Request.cs
@@ -4,12 +4,34 @@ using System.Threading;
 
 namespace WealthFarm.SparkPost
 {
+    /// <summary>
+    ///     Request is a generic envelope used to describe a SparkPost request.
+    /// </summary>
     public class Request
     {
+        /// <summary>
+        ///     Gets or sets the HTTP method.
+        /// </summary>
         public HttpMethod Method { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the relative URI for the request.
+        /// </summary>
         public Uri Uri { get; set; }
+
+        /// <summary>
+        ///     Gets or sets an optional content entity.
+        /// </summary>
         public object Content { get; set; }
+
+        /// <summary>
+        ///     Gets or sets an optional cancellation token.
+        /// </summary>
         public CancellationToken CancellationToken { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the HTTP completion option.
+        /// </summary>
         public HttpCompletionOption CompletionOption { get; set; }
     }
 }
